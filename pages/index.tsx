4bb6f20d7d73f099a7e225fb7750e076a4e6e1a0
kenmueller <kenmueller0@gmail.com>
2021-02-05 16:02:23 -0800

    boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import { NextPage } from 'next'
 
 import styles from 'styles/Home.module.scss'
 
-const Home: NextPage = () => (
-	<h1 className={styles.title}>Hello, world</h1>
-)
+const Home: NextPage = () => <h1 className={styles.title}>Hello, world</h1>
 
 export default Home
